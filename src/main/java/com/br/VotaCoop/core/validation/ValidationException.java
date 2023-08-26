4d8ba51073e8084e06b0fd87a849028e4966b1f4
João Victor <jvdematos004@gmail.com>
2023-08-26 12:40:49 -0300

    feat: adiciona exceção personalizada para validações

---
+++ src/main/java/com/br/VotaCoop/core/validation/ValidationException.java
@@ -0,0 +1,15 @@
+package com.br.VotaCoop.core.validation;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.validation.BindingResult;
+
+@AllArgsConstructor
+@Getter
+public class ValidationException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    private BindingResult bindingResult;
+
+}
