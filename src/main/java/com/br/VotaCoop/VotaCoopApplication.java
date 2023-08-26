616cde26636114f1d9d4e8072ca60c2e699a5329
João Victor <jvdematos004@gmail.com>
2023-08-26 10:36:14 -0300

    initial commit

---
+++ src/main/java/com/br/VotaCoop/VotaCoopApplication.java
@@ -0,0 +1,13 @@
+package com.br.VotaCoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotaCoopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotaCoopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/VotaCoop/VotaCoopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.VotaCoop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotaCoopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
